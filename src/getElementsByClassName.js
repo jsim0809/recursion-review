7bcf8d583bfa0232707a072fbcb165a3de31b12b
jsim0809 <jeremy_sim@hotmail.com>
2020-02-03 15:14:54 -0800

    SUCCESS!!!

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -7,4 +7,37 @@
 var getElementsByClassName = function(className
 ) {
   // your code here
+  // document.body is the <body> text of the HTML
+  // element.childNodes returns a NodeList (like an array) of the children nodes
+  // element.classList returns a DOMTokenList (space separated "string") of the node's classes
+  var result = [];
+  var currentElement = document.body;
+
+  var getKids = function(element) {
+    if (element.classList === undefined) {
+
+    } else if (element.classList.contains(className)) {
+      result.push(element);
+    }
+
+    var children = element.childNodes;
+
+    if (children.length > 0) {
+      for (var i = 0; i < children.length; i++) {
+        getKids(children[i]);
+      }
+    }
+  };
+
+  getKids(currentElement);
+  result.flat(Infinity);
+
+  var expectedNodeList = document.getElementsByClassName('targetClassName');
+  var expectedArray = Array.prototype.slice.apply(expectedNodeList);
+  console.log(expectedArray);
+  console.log(result);
+
+
+  return result;
+
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,41 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+
+  if (typeof obj === 'function' || obj === undefined) {
+    return '';
+  } else if (obj === NaN || obj === null || obj === Infinity) {
+    return 'null';
+  } else if (typeof obj === 'string') {
+    return '"' + obj.toString() + '"';
+  } else if (typeof obj === 'boolean' || typeof obj === 'number') {
+    return obj.toString();
+  } else if (Array.isArray(obj)) {
+    var middle = '';
+
+    for (var i = 0; i < obj.length; i++) {
+      if (i === obj.length - 1) {
+        middle += stringifyJSON(obj[i]);
+      } else {
+        middle += stringifyJSON(obj[i]) + ',';
+      }
+    }
+    return '[' + middle + ']';
+
+  } else if (typeof obj === 'object') {
+    var middle = '';
+    var counter = 0;
+    for (var key in obj) {
+      if (typeof obj[key] === 'function' || obj[key] === undefined) {
+      } else if (counter === Object.keys(obj).length - 1) {
+        middle += '"' + key + '\":' + stringifyJSON(obj[key]);
+      } else {
+        middle += '"' + key + '\":' + stringifyJSON(obj[key]) + ',';
+      }
+      counter++;
+    }
+    return '{' + middle + '}';
+  } else {
+    return;
+  }
 };
